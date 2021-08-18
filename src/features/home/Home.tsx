7977072cf3a535c7fb5cc71f6c82371ba5dfc5e7
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-18 21:29:20 +0530

    tailwind and styled components added

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const PageContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
+`;
+
 export default function Home() {
     return (
-        <div>
+        <PageContainer>
             Home
-        </div>
+        </PageContainer>
     )
 }
