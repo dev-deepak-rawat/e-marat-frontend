7aa68c6859d67db71f3cb74f1a2992c541b8e3cf
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-21 14:11:12 +0530

    manual src path import configure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import ErrorBoundary from './app/ErrorBoundary';
 import { ToastContainer } from 'react-toastify';
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
-import ManageUser from './features/manageUser/ManageUser';
-import Home from './features/home/Home';
-import ManageAmenities from './features/manageAmenities/ManageAmenities';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 'index.css';
+import * as serviceWorker from 'serviceWorker';
+import { store } from 'app/store';
+import ErrorBoundary from 'app/ErrorBoundary';
+import ManageUser from 'features/manageUser/ManageUser';
+import Home from 'features/home/Home';
+import ManageAmenities from 'features/manageAmenities/ManageAmenities';
 
 Sentry.init({
     dsn: "https://1ef6e7d163ff41eeb401880603c323da@o960298.ingest.sentry.io/5912682",
