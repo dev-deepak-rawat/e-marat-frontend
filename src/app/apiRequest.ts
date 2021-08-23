a3d960cc753141bb96d41571a3489a302092b5fd
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-23 16:05:38 +0530

    apiRequest method added

--- src/app/apiRequest.ts
+++ src/app/apiRequest.ts
@@ -1,6 +1,47 @@
-type ParamsTypes = {
-    options: {};
-    api: string;
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { API_CONFIG, SERVICE_URL } from 'lib/constants';
+import { getAuthToken } from 'lib/utils';
+import type { AnyMapObj, StringMapObj } from './types';
+
+type BuildRequestDataType = {
+    apiUrl: keyof typeof API_CONFIG;
+    headers?: StringMapObj;
+    data?: AnyMapObj;
 };
 
-export const apiRequest = (params: ParamsTypes) => {};
+const buildRequestData = (options: BuildRequestDataType) => {
+    const { apiUrl, headers = {}, data } = options;
+    const { url, method } = API_CONFIG[apiUrl];
+    const reqUrl = `${SERVICE_URL}${url}`;
+    const token = getAuthToken();
+    const reqHeaders = {
+        'Content-Type': 'application/json',
+        Authorizatoin: token ? `Bearer ${token}` : undefined,
+        ...headers,
+    };
+
+    return {
+        url: reqUrl,
+        headers: reqHeaders,
+        data: JSON.stringify(data),
+        method,
+    }
+}
+
+export const apiRequest = async (options: BuildRequestDataType) => {
+    const reqData = buildRequestData(options);
+    try {
+        const response = await axios(reqData);
+        const jsonResponse = await response.data;
+        return jsonResponse;
+    } catch (err) {
+        if (err.response) {
+            const { data = {} } = err.response;
+            const { meta = {} } = data;
+            const { msg = '' } = meta;
+            toast.error(msg || 'Something Went Wrong');
+        }
+    }
+    return {};
+};
---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+import { Method } from "axios";
+
+export const SERVICE_URL = 'http://localhost:5000';
+
+type ApiConfigType = { [key: string]: { url: string, method: Method } };
+
+export const API_CONFIG: ApiConfigType = {
+    postUser: {
+        url: '/users/',
+        method: 'POST'
+    }
+}
