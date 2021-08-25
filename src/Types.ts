4559ade9e9e9667554d28440ea81a3195ff4dbed
Haris Rahman <iharisrahman@yahoo.com>
2021-08-25 20:11:42 +0530

    es lint reverted

--- src/Types.ts
+++ src/Types.ts
@@ -1,11 +1,12 @@
-export type ColorType = 'primary' | 'danger' | 'warning' | 'success' | 'emarat-accent';
+export type ColorType =
+	| 'primary'
+	| 'danger'
+	| 'warning'
+	| 'success'
+	| 'emarat-accent';
 
-
-export type ColorMap = {	
+export type ColorMap = {
 	[key in ColorType]?: string;
 };
 
 // export type ColorMap = { [key: string]: string; };
-
-
-
