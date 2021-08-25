c66bcf2bc43d43ae1c9b0e3af014c1cda2ef3ada
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-26 01:24:06 +0530

    title and sider button added

--- src/features/navigations/Sidebar.tsx
+++ src/features/navigations/Sidebar.tsx
@@ -4,13 +4,27 @@ import { menuData } from 'features/navigations/menuData';
 import MenuItem from 'features/navigations/MenuItem';
 import { useAuth, useOrientation } from 'app/hooks';
 import { getDefaultSelectedKeys } from 'features/navigations/menuHelper';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
+const Logo = styled.div`
+	${tw`
+        h-16
+        bg-gray-300
+        text-center
+        text-2xl
+        pt-3
+        font-bold
+        text-emarat-secondary-default
+    `}
+`;
+
 export default function Sidebar() {
-	const [collapsed, onCollapse] = useState(false);
 	const { isMobile } = useOrientation();
+	const [collapsed, onCollapse] = useState(isMobile);
 	const { filterByRole } = useAuth();
 	const filteredMenuData = menuData.filter(filterByRole);
 	const { defaultOpenKeys, defaultSelectedKeys } =
@@ -23,8 +37,15 @@ export default function Sidebar() {
 			onCollapse={onCollapse}
 			collapsedWidth={isMobile ? 0 : 80}
 			theme="dark"
+			zeroWidthTriggerStyle={{
+				top: 0,
+				height: '3rem',
+				right: '-50px',
+				width: 50,
+			}}
 		>
-			<div className="logo">xx-Logo Here-xx</div>
+			{/* <div className="logo">xx-Logo Here-xx</div> */}
+			{!isMobile && <Logo>{collapsed ? 'E' : 'E-marat'}</Logo>}
 			<Menu
 				theme="dark"
 				defaultSelectedKeys={defaultSelectedKeys}
--- src/features/navigations/Topbar.tsx
+++ src/features/navigations/Topbar.tsx
@@ -1,5 +1,17 @@
-import { Header } from 'antd/lib/layout/layout';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { menuData } from 'features/navigations/menuData';
+import { getPageTitle } from 'features/navigations/menuHelper';
+
+const Header = styled.div`
+	${tw`
+        flex
+        bg-gray-200
+        h-12
+    `}
+`;
 
 export default function Topbar() {
-	return <Header className="site-layout-background" style={{ padding: 0 }} />;
+	const title = getPageTitle(menuData);
+	return <Header>{title}</Header>;
 }
