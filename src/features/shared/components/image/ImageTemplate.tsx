f4eef0db2f532105db9667149c14d6589bd6efc7
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-08-29 17:17:17 +0530

    UPDATE: cross button on image

--- src/features/shared/components/image/ImageTemplate.tsx
+++ src/features/shared/components/image/ImageTemplate.tsx
@@ -4,19 +4,19 @@ import uploadIcon from 'assets/images/upload-icon.svg';
 import ImageCentered from 'features/shared/components/styledComponents/ImageCentered.style';
 
 export default function ImageTemplate() {
-    const { imageUrl,
-        isImageLoading,
-        handleImageLoad,
-        clearImage
-    } = useImage();
+	const { imageUrl, isImageLoading, handleImageLoad, clearImage } =
+		useImage();
 	return (
 		<div className="mx-auto">
-            {imageUrl &&
-                <button
-                    type="button"
-                    onClick={() => clearImage()}
-                    className="text-lg font-medium hover:text-gray-500 absolute right-4 top-0 z-10"
-                >x</button>}
+			{imageUrl && (
+				<button
+					type="button"
+					onClick={() => clearImage()}
+					className="text-lg font-medium hover:text-gray-500 absolute right-4 top-0 z-10"
+				>
+					x
+				</button>
+			)}
 			{imageUrl ? (
 				<ImageCentered
 					src={imageUrl}
@@ -31,13 +31,13 @@ export default function ImageTemplate() {
 						alt="Upload"
 						width="200"
 					/>
-                        <div className="ant-upload-text mt-3">
+					<div className="ant-upload-text mt-3">
 						{isImageLoading ? (
 							<Spin />
 						) : (
 							<>Click or drag file to this area to upload</>
 						)}
-                        </div>
+					</div>
 				</>
 			)}
 		</div>
