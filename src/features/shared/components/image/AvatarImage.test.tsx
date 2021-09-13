8a98d58ea07d0408a2ccdb7d0ef40bf90d396432
Deepak Rawat <deepak.singh@technogramsolutions.com>
2021-09-14 00:17:56 +0530

    UPDATE: dashboard to align style improved

--- src/features/shared/components/image/AvatarImage.test.tsx
+++ src/features/shared/components/image/AvatarImage.test.tsx
@@ -1,9 +1,10 @@
 import AvatarImage from 'features/shared/components/image/AvatarImage';
+import { TEST_IMG_URL } from 'lib/constants';
 import renderer from 'react-test-renderer';
 
 it('Image Template', () => {
-	const userImg =
-		'https://res.cloudinary.com/emarat/image/upload/v1631362403/dfejes8g9doeysyniydd.png';
-	const tree = renderer.create(<AvatarImage userImg={userImg} />).toJSON();
+	const tree = renderer
+		.create(<AvatarImage userImg={TEST_IMG_URL} />)
+		.toJSON();
 	expect(tree).toMatchSnapshot();
 });
