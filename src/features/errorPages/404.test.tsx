779e639f206056836ed078a7d1440606c3d1172c
Deepak Singh Rawat <dev.deepak.rawat@gmail.com>
2021-09-15 18:27:08 +0530

    404 snapshot updated

--- src/features/errorPages/404.test.tsx
+++ src/features/errorPages/404.test.tsx
@@ -1,7 +1,15 @@
 import Page404 from 'features/errorPages/404';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { store } from 'config/store';
 
 it('404 error page', () => {
-	const tree = renderer.create(<Page404 />).toJSON();
+	const tree = renderer
+		.create(
+			<Provider store={store}>
+				<Page404 />
+			</Provider>
+		)
+		.toJSON();
 	expect(tree).toMatchSnapshot();
 });
