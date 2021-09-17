f66e13102af7f23a7712f71a233622ec3dd61e43
Deepak Singh Rawat <dev.deepak.rawat@gmail.com>
2021-09-18 00:46:55 +0530

    testing react lazy

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -1,12 +1,13 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import ProtectedRoute from 'config/ProtectedRoute';
 import ManageUsers from 'features/users/ManageUsers';
 import Home from 'features/home/Home';
 import ManageAmenities from 'features/amenities/ManageAmenities';
 import MyAmenities from 'features/amenities/MyAmenities';
-import MyPayments from 'features/payments/MyPayments';
-import Dashboard from 'features/dashboard/Dashboard';
-import SocialFeed from 'features/socialFeed/SocialFeed';
+// import MyPayments from 'features/payments/MyPayments';
+// import Dashboard from 'features/dashboard/Dashboard';
+// import SocialFeed from 'features/socialFeed/SocialFeed';
 import ManageComplaints from 'features/complaints/ManageComplaints';
 import MyComplaints from 'features/complaints/MyComplaints';
 import Transactions from 'features/payments/Transactions';
@@ -17,14 +18,20 @@ import Profile from 'features/profile/Profile';
 import { useAuth } from 'config/hooks';
 import Announcements from 'features/announcements/Announcements';
 
+const MyPayments = lazy(() => import('features/payments/MyPayments'));
+const Dashboard = lazy(() => import('features/dashboard/Dashboard'));
+const SocialFeed = lazy(() => import('features/socialFeed/SocialFeed'));
+
 export default function Routes() {
 	const { isAdmin } = useAuth();
 
 	return (
+		<Suspense fallback={<div>loading...</div>}>
 		<Switch>
 			<Route path="/" exact>
 				<Home />
 			</Route>
+			
 			<Route path="/404" exact>
 				<PageNotFound />
 			</Route>
@@ -39,24 +46,34 @@ export default function Routes() {
 				component={isAdmin ? ManageAmenities : MyAmenities}
 				exact
 			/>
-			<ProtectedRoute
-				path="/payments"
-				role={ROLES.RESIDENT}
-				component={MyPayments}
-				exact
-			/>
+			
 			<ProtectedRoute
 				path="/transactions"
 				role={ROLES.ADMIN}
 				component={Transactions}
 				exact
 			/>
+			
 			<ProtectedRoute
 				path="/dashboard"
 				role={ROLES.ADMIN}
 				component={Dashboard}
 				exact
 			/>
+			<ProtectedRoute 
+			path="/social-feed"
+			component={
+				SocialFeed
+			}
+			exact
+			/>
+			<ProtectedRoute
+				path="/payments"
+				role={ROLES.RESIDENT}
+				component={MyPayments}
+				exact
+			/>
+			
 			<ProtectedRoute
 				path="/complaints"
 				role=""
@@ -69,10 +86,12 @@ export default function Routes() {
 				component={Announcements}
 				exact
 			/>
-			<ProtectedRoute path="/social-feed" component={SocialFeed} exact />
+			
 			<ProtectedRoute path="/profile" component={Profile} exact />
 			<ProtectedRoute path="/broadcasts" component={Broadcasts} exact />
 			<Redirect from="*" to="/404" />
+			
 		</Switch>
+		</Suspense>
 	);
 }
