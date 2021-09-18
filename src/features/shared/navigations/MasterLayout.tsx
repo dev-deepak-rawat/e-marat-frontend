975cd854930335e187acca78c39c1dfb2598968f
Haris Rahman <iharisrahman@yahoo.com>
2021-09-18 19:37:13 +0530

    login/logout optimised

--- src/features/shared/navigations/MasterLayout.tsx
+++ src/features/shared/navigations/MasterLayout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { Layout } from 'antd';
 import Sidebar from 'features/shared/navigations/Sidebar';
 import Main from 'features/shared/navigations/Main';
@@ -6,11 +6,9 @@ import Topbar from 'features/shared/navigations/Topbar';
 import { useLocation } from 'react-router-dom';
 import { useOrientation } from 'config/hooks';
 import { SiteLayout } from 'features/shared/components/styledComponents/SiteLayout';
-import { IsOnlineContext } from 'config/IsOnlineContext';
 import { toast } from 'react-toastify';
 
 export default function MasterLayout(props: PropsWithChildren<{}>) {
-	const [isOnline, setIsOnline] = useState(navigator.onLine);
 	const { isMobileSize } = useOrientation();
 	const [collapsed, onCollapse] = useState(isMobileSize);
 	const location = useLocation();
@@ -18,11 +16,9 @@ export default function MasterLayout(props: PropsWithChildren<{}>) {
 
 	const networkChangeEvent = () => {
 		if (navigator.onLine) {
-			setIsOnline(true);
 			toast.dismiss();
 			toast.success('Back Online!');
 		} else {
-			setIsOnline(false);
 			toast.dismiss();
 			toast.error("You're Offline!");
 		}
@@ -32,13 +28,11 @@ export default function MasterLayout(props: PropsWithChildren<{}>) {
 
 	return (
 		<Layout className="min-h-screen">
-			<IsOnlineContext.Provider value={isOnline}>
-				<SiteLayout collapsed={collapsed}>
-					<Topbar />
-					<Main {...props} />
-				</SiteLayout>
-				<Sidebar {...{ collapsed, onCollapse, isMobileSize }} />
-			</IsOnlineContext.Provider>
+			<SiteLayout collapsed={collapsed}>
+				<Topbar />
+				<Main {...props} />
+			</SiteLayout>
+			<Sidebar {...{ collapsed, onCollapse, isMobileSize }} />
 		</Layout>
 	);
 }
